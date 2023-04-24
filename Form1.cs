d834c53e5a99df61332edd3faba942845573fa0c
kr3go6 <kr3go6@gmail.com>
2023-04-24 21:47:50 +0300

    Add files browsing in mainform

--- Form1.cs
+++ Form1.cs
@@ -15,47 +15,16 @@ namespace WinPsychTest
     public partial class Form1 : Form
     {
         Uri authorizeUri;
+        public static string apiToken = "";
+        static readonly string appKey = "XXXXXXXXXXXXXXX";
+        static readonly string appSecret = "XXXXXXXXXXXXXXX";
 
-        async Task ListRootFolder(DropboxClient dbx)
+        static async Task<string> Login(string code)
         {
-            var list = await dbx.Files.ListFolderAsync(string.Empty);
-
-            // show folders then files
-            foreach (var item in list.Entries.Where(i => i.IsFolder))
+            using (var client = new DropboxClient(appKey, appSecret))
             {
-                Console.WriteLine("D  {0}/", item.Name);
-            }
-
-            foreach (var item in list.Entries.Where(i => i.IsFile))
-            {
-                Console.WriteLine("F{0,8} {1}", item.AsFile.Size, item.Name);
-            }
-        }
-
-        static async Task<string> Run()
-        {
-            using (var dbx = new DropboxClient("sl.Bc_KM_hoHJEdf_iOZzc2YYFe0N0kR21k6rd1q03GdbQAsKuJO6Ovdfg_U2hPxiY9mjNxCZ8h8rVkbwpFrSBttHboH524y0R4Mx45b2eap4NXKyluQpuHeTlsHVC10dXP9hh6sCU"))
-            {
-                var folder = "/data/data/com.example.testapplication/cache";
-                var full = await dbx.Users.GetCurrentAccountAsync();
-                MessageBox.Show(string.Format("{0} - {1}", full.Name.DisplayName, full.Email));
-
-                var list = await dbx.Files.ListFolderAsync(folder);
-
-                string files = "";
-
-                // show folders then files
-                foreach (var item in list.Entries.Where(i => i.IsFolder))
-                {
-                    files += string.Format("D  {0}/\r\n", item.Name);
-                }
-
-                foreach (var item in list.Entries.Where(i => i.IsFile))
-                {
-                    files += string.Format("F{0,8} {1}\r\n", item.AsFile.Size, item.Name);
-                }
-
-                return files;
+                var token = await DropboxOAuth2Helper.ProcessCodeFlowAsync(code, appKey, appSecret);
+                return token.AccessToken;
             }
         }
 
@@ -63,19 +32,27 @@ namespace WinPsychTest
         {
             InitializeComponent();
 
-            authorizeUri = DropboxOAuth2Helper.GetAuthorizeUri(OAuthResponseType.Code, "XXXXXXXXXXXXXXX", (Uri)null);
+            authorizeUri = DropboxOAuth2Helper.GetAuthorizeUri(OAuthResponseType.Code, appKey, (Uri)null);
 
             Console.WriteLine(authorizeUri);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (textBox1.Text == "")
+            {
+                return;
+            }
 
-            Task<string> task = Task.Run((Func<Task<string>>)Form1.Run);
+            Task<string> task = Task.Run(() => (Task<string>)Login(textBox1.Text));
             task.Wait();
 
-            //textBox2.Text = task.GetAwaiter()
-            //                    .GetResult();
+            apiToken = task.GetAwaiter()
+                                .GetResult();
+
+            Form form2 = new MainForm();
+            this.Hide();
+            form2.ShowDialog();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
---
+++ MainForm.cs
@@ -0,0 +1,62 @@
+﻿using Dropbox.Api;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WinPsychTest
+{
+    public partial class MainForm : Form
+    {
+        static string apiToken;
+
+        static async Task<string> Run()
+        {
+            using (var dbx = new DropboxClient(apiToken))
+            {
+                var folder = "/data/data/com.example.testapplication/cache";
+                var list = await dbx.Files.ListFolderAsync(folder);
+
+                string files = "";
+
+                foreach (var item in list.Entries.Reverse().Where(i => i.IsFile))
+                {
+                    files += string.Format("{0,19}   {1}\r\n", item.AsFile.ClientModified.ToString(), item.Name);
+                }
+
+                return files;
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            apiToken = Form1.apiToken;
+
+            Task<string> task = Task.Run(Run);
+            task.Wait();
+
+            richTextBox1.Text = task.GetAwaiter()
+                                .GetResult();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task<string> task = Task.Run(Run);
+            task.Wait();
+
+            richTextBox1.Text = task.GetAwaiter()
+                                .GetResult();
+        }
+    }
+}
